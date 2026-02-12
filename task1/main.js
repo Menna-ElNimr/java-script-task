177911ede8bc3408e5ccef8339c500af81e209a9
Menna-ElNimr <mennaalaa1474@gmail.com>
2026-02-12 15:54:31 +0200

    calculator task

---
+++ task1/main.js
@@ -0,0 +1,24 @@
+let num1 = 7;
+let operator = "/";
+let num2 = 2;
+
+let result;
+
+if (operator === "+") {
+    result = num1 + num2;
+} 
+else if (operator === "-") {
+    result = num1 - num2;
+} 
+else if (operator === "*") {
+    result = num1 * num2;
+} 
+else if (operator === "/") {
+   
+        result = num1 / num2;
+    } 
+else {
+    result = "Invalid operator";
+}
+
+console.log("Result =", result);
